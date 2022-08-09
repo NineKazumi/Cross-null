6e1d08750e357d2f100fc7789b837ea4d7596f81
Full <dix.luffy@mail.ru>
2022-08-09 18:48:38 +0300

    Версия 0.1

--- JavaScript.js
+++ JavaScript.js
@@ -1,94 +1,161 @@
 var whoseMotion = 0
-var plusOneEveryMotion = 0
+var cellGame = ["","","",
+				"","","",
+				"","",""]
 function crossNullMotionDrawWinClick(cell){	
 	//Переменные
-	 	const playerX = ["Выйграл игрок 1(Х)","Сейчас ход игрока 1(Х)","X"]
-		const player0 = ["Bыйграл игрок 2(0)","Сейчас ход игрока 2(0)","0"]
+	 	const playerX = ["Выйграл игрок 1(Х)","Сейчас ход игрока 2(0)","X"]
+		const player0 = ["Bыйграл игрок 2(0)","Сейчас ход игрока 1(X)","0"]
 		var motion = document.getElementById("whoseMotionWin")
 		var pasteButtonX0 = document.getElementById(cell)
-		var cellGame =
-	   [["CellGame0"[""]],["CellGame1"[""]],["CellGame2"[""]],
-		["CellGame3"[""]],["CellGame4"[""]],["CellGame5"[""]],
-		["CellGame6"[""]],["CellGame7"[""]],["CellGame8"[""]]]
 	//Функция хода
+		//Ход Х
+			if (whoseMotion == 0
+			 || whoseMotion == 2
+			 || whoseMotion == 4
+			 || whoseMotion == 6
+			 || whoseMotion == 8) {
+				pasteButtonX0.innerHTML = playerX[2]
+				motion.innerHTML = playerX[1]
+					if (cell == "cellGame0") {
+						cellGame[0] = playerX[2]
+					}
+					else if (cell == "cellGame1") {
+						cellGame[1] = playerX[2]
+					}
+					else if (cell == "cellGame2") {
+						cellGame[2] = playerX[2]
+					}
+					else if (cell == "cellGame3") {
+						cellGame[3] = playerX[2]
+					}
+					else if (cell == "cellGame4") {
+						cellGame[4] = playerX[2]
+					}
+					else if (cell == "cellGame5") {
+						cellGame[5] = playerX[2]
+					}
+					else if (cell == "cellGame6") {
+						cellGame[6] = playerX[2]
+					}
+					else if (cell == "cellGame7") {
+						cellGame[7] = playerX[2]
+					}
+					else if (cell == "cellGame8") {
+						cellGame[8] = playerX[2]
+					}
+			}
+				//ПобедаX
+				if (cellGame[0] == playerX[2] & cellGame[1] == playerX[2] & cellGame[2] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[3] == playerX[2] & cellGame[4] == playerX[2] & cellGame[5] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[6] == playerX[2] & cellGame[7] == playerX[2] & cellGame[8] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[0] == playerX[2] & cellGame[3] == playerX[2] & cellGame[6] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[1] == playerX[2] & cellGame[4] == playerX[2] & cellGame[7] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[2] == playerX[2] & cellGame[5] == playerX[2] & cellGame[8] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[0] == playerX[2] & cellGame[4] == playerX[2] & cellGame[8] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+				if (cellGame[6] == playerX[2] & cellGame[4] == playerX[2] & cellGame[2] == playerX[2]) {
+					pasteButtonX0.innerHTML = playerX[1]
+					window.location.href = 'win1.html'
+				}
+		//Ход 0
+			else if (whoseMotion == 1 
+				  || whoseMotion == 3 
+			 	  || whoseMotion == 5
+			 	  || whoseMotion == 7
+				  || whoseMotion == 9) {
+				pasteButtonX0.innerHTML = player0[2]
+				motion.innerHTML = player0[1]
+					if (cell == "cellGame0") {
+						cellGame[0] = player0[2]
+					}
+					else if (cell == "cellGame1") {
+						cellGame[1] = player0[2]
+					}
+					else if (cell == "cellGame2") {
+						cellGame[2] = player0[2]
+					}
+					else if (cell == "cellGame3") {
+						cellGame[3] = player0[2]
+					}
+					else if (cell == "cellGame4") {
+						cellGame[4] = player0[2]
+					}
+					else if (cell == "cellGame5") {
+						cellGame[5] = player0[2]
+					}
+					else if (cell == "cellGame6") {
+						cellGame[6] = player0[2]
+					}
+					else if (cell == "cellGame7") {
+						cellGame[7] = player0[2]
+					}
+					else if (cell == "cellGame8") {
+						cellGame[8] = player0[2]
+					}
+			}
+			//Победа0
+				if (cellGame[0] == player0[2] & cellGame[1] == player0[2] & cellGame[2] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[3] == player0[2] & cellGame[4] == player0[2] & cellGame[5] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[6] == player0[2] & cellGame[7] == player0[2] & cellGame[8] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[0] == player0[2] & cellGame[3] == player0[2] & cellGame[6] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[1] == player0[2] & cellGame[4] == player0[2] & cellGame[7] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[2] == player0[2] & cellGame[5] == player0[2] & cellGame[8] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[0] == player0[2] & cellGame[4] == player0[2] & cellGame[8] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
+				if (cellGame[6] == player0[2] & cellGame[4] == player0[2] & cellGame[2] == player0[2]) {
+					pasteButtonX0.innerHTML = player0[1]
+					window.location.href = 'win2.html'
+				}
 		//+ ход
 			whoseMotion++
 			console.log(whoseMotion)
-		//Ход Х
-			if (whoseMotion == 1 
-			 || whoseMotion == 3 
-			 || whoseMotion == 5
-			 || whoseMotion == 7
-			 || whoseMotion == 9) {
-				pasteButtonX0.innerHTML = playerX[2]
-				motion.innerHTML = playerX[1]
-				//ПобедаX
-					if (cell == "cellGame0") {
-						cellGame[0[1]] = "x"
-					}
-					else if (cell == "cellGame1") {
-						cellGame[1[1]] = "x"
-					}
-					else if (cell == "cellGame2") {
-						cellGame[2[1]] = "x"
-					}
-					else if (cell == "cellGame3") {
-						cellGame[3[1]] = "x"
-					}
-					else if (cell == "cellGame4") {
-						cellGame[4[1]] = "x"
-					}
-					else if (cell == "cellGame5") {
-						cellGame[5[1]] = "x"
-					}
-					else if (cell == "cellGame6") {
-						cellGame[6[1]] = "x"
-					}
-					else if (cell == "cellGame7") {
-						cellGame[7[1]] = "x"
-					}
-					else if (cell == "cellGame8") {
-						cellGame[8[1]] = "x"
-					}
+			console.log(cellGame)
+		//Hичья
+			if (whoseMotion == 9) {
+				window.location.href = 'draw.html'
 			}
-		//Ход 0
-			else if (whoseMotion == 2
-			 	  || whoseMotion == 4
-				  || whoseMotion == 6
-				  || whoseMotion == 8) {
-				pasteButtonX0.innerHTML = player0[2]
-				motion.innerHTML = player0[1]
-				//Победа0
-					if (cell == "cellGame0") {
-						cellGame[0[1]] = "0"
-					}
-					else if (cell == "cellGame1") {
-						cellGame[1[1]] = "0"
-					}
-					else if (cell == "cellGame2") {
-						cellGame[2[1]] = "0"
-					}
-					else if (cell == "cellGame3") {
-						cellGame[3[1]] = "0"
-					}
-					else if (cell == "cellGame4") {
-						cellGame[4[1]] = "0"
-					}
-					else if (cell == "cellGame5") {
-						cellGame[5[1]] = "0"
-					}
-					else if (cell == "cellGame6") {
-						cellGame[6[1]] = "0"
-					}
-					else if (cell == "cellGame7") {
-						cellGame[7[1]] = "0"
-					}
-					else if (cell == "cellGame8") {
-						cellGame[8[1]] = "0"
-					}
-
-			}
-		console.log(cellGame[0[1]])
-	//	console.log(plusOneEveryMotion)
-		plusOneEveryMotion++
+}
+function home(){
+	window.location = 'index.html'
 }
